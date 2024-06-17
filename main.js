ca0236ed0ea15ea3d277453cc51de0ecdb80b6b4
Daniely1103 <daniely.correia@escola.pr.gov.br>
2024-06-17 11:50:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -43,5 +43,30 @@ function gerasenha() {
     if (checkbox[1].checked) {
         alfabeto = alfabeto + numeros;
     }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = math.radom() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha= senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.valeue = senh;
+    classificaSenha(alfabeto.length);
+}function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.logg(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if(entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add ('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "um computador pode levar ate" + Math.floor(2**entropia/(100e60*60*24)) + "dias p"
+}
     
 
